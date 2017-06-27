1cca41127a4214d19598a0ce793cf50f8e13d465
dani1235a <dani1235a@gmail.com>
2017-06-27 12:55:46 -0700

    made skeleton for router

--- 430Project/src/Router.java
+++ 430Project/src/Router.java
@@ -9,32 +9,46 @@ public class Router {
     private String routerName;
 
     /**
-     *
-     * @param routerName
+     * Constructor that initiallizes the router.
+     * @param routerName - name associated with router.
      */
     public Router(String routerName) {
         this.routerName = routerName;
     }
 
+    /**
+     * Inner class with RouterTable
+     */
     public class RouterTable {
-
+        /**
+         * The 2d array that stores all the router data.
+         */
         private String [][] routerTable;
 
         /**
-         *
+         * Private Constructor
          */
-        private RouterTable() {
+        private RouterTable() {}
 
-        }
-
+        /**
+         * Method to print out the table in fancy manner.
+         * @return table with all the data in it
+         */
         public String printRouterTable() {
             return "print fancy table here";
         }
 
+        /**
+         * Method to add a device to table.
+         */
         public void addToTable() {
 
         }
 
+        /**
+         * Method to remove specified device from the table
+         * @param ip ip address associted with the specific device
+         */
         public void removeFromTable(String ip) {
 
         }
